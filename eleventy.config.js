cf516c7b6ccd4a2ff405db16554b7bc22494b564
Emma Azelborn <emmaazelborn@gmail.com>
2025-07-10 17:31:52 -0700

    smaller floral print size

--- eleventy.config.js
+++ eleventy.config.js
@@ -81,7 +81,20 @@ export default async function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/static")
 
     // optimize image sizes: https://www.11ty.dev/docs/plugins/image/#html-transform
-    eleventyConfig.addPlugin(eleventyImageTransformPlugin);
+    eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
+        // which file extensions to process
+        extensions: 'html',
+        // optional, output image formats
+        formats: ['jpg', 'webp'],
+        // optional, output image widths
+        widths: ['auto', 400, 800],
+        // optional, attributes assigned on <img> override these values.
+        defaultAttributes: {
+            loading: 'lazy',
+            sizes: '100vw',
+            decoding: 'async',
+        },
+    });
 }
 
 export const config = {
